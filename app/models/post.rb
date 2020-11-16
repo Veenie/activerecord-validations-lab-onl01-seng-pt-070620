e9cc4fc56852590be1d180f97852a1b197a773b0
Raveen K Sharma <raveen08@gmail.com>
2020-11-16 23:00:00 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ActiveRecord::Base
   validates :title, presence: true 
   validates :content, presence: true, length: { minimum: 250 }
   validates :summary, presence: true, length: { maximum: 250 }
-  
+  validates :category, presence: true, 
 end
