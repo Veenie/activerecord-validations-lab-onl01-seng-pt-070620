36d7aac6c0482c724fc1c290c7503d97f47553b0
Raveen K Sharma <raveen08@gmail.com>
2020-11-16 23:15:39 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ActiveRecord::Base
   validates :content, presence: true, length: { minimum: 250 }
   validates :summary, presence: true, length: { maximum: 250 }
   validates :category, presence: true, inclusion: { in: %w(Fiction non-fiction) }
-  validates :clickbait, on: create
+  validate :clickbait, on: create
   
   CLICKBAIT_PATTERNS = [
     /Won't Believe/i,
